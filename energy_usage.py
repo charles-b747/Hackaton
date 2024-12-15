1b04d788643d851b54bb8005d419f6c5cda6001c
charles-b747 <b747.programmer@gmail.com>
2024-12-15 14:08:28 +0100

    Added tips to end.html

--- energy_usage.py
+++ energy_usage.py
@@ -4,31 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-#def result_calculate(distance, gas, consumption):
-# Zmienne umożliwiające obliczenie poboru energii przez urządzenia
- #   if distance == "150 km lub mniej":
-  #      distance = 150
-   # elif distance == "500 km":
-    #    distance = 500
-   # elif distance == "1000 km lub więcej":
-    #    distance = 1000
-        
-    #if gas == "Benzyna":
-      #  gas = 2392
-    #elif gas == "Diesel":
-      #  gas = 2650
-    #elif gas == "Hybryd":
-     #   gas = 2200
-    
-    #if consumption == "5L/100km lub więcej":
-     #   consumption = 0.05
-    #elif consumption == "ok. 8L/100km":
-      #  consumption = 0.08
-    #elif consumption == "10L/100km lub więcej":
-     #   consumption = 0.1
-
-   # return (distance * consumption * gas)/1000
-
 def result_calculate(distance, gas, consumption):
     if distance == 1:
         distance = 150
